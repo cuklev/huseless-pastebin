ac177e62d294c95adc4987211fbee8306aabd41c
Kristiyan Tsaklev <cuki@cukii.tk>
2016-11-06 00:58:27 +0200

    db settings

--- Foundation.hs
+++ Foundation.hs
@@ -2,12 +2,36 @@
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE ViewPatterns      #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE MultiParamTypeClasses   #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Foundation where
 
 import Yesod.Core
+import Yesod.Persist
+import Data.Text (Text)
+import Database.Persist.Sqlite
 
-data App = App
+data App = App { connPool :: ConnectionPool}
 
 mkYesodData "App" $(parseRoutesFile "config/routes")
 
 instance Yesod App
+
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Paste
+    filename Text
+    contents Text
+    viewId   Text
+    editId   Text
+|]
+
+instance YesodPersist App where
+    type YesodPersistBackend App = SqlBackend
+    runDB f = do
+        master <- getYesod
+        let pool = connPool master
+        runSqlPool f pool
--- Main.hs
+++ Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Application () -- for YesodDispatch instance
 import Foundation
 import Yesod.Core
+import Database.Persist.Sqlite
+import Control.Monad.Logger (runStdoutLoggingT)
 
 main :: IO ()
-main = warp 3000 App
+main = do
+    pool <- runStdoutLoggingT $ createSqlitePool "pastebin.db3" 10
+    warp 3000 $ App { connPool = pool }
